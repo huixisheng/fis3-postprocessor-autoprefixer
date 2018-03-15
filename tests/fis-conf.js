2f97b6c4feb57742c980672ba66078e89731e843
huixisheng <huixisheng@huixishengdeMacBook-Pro.local>
2018-03-15 09:54:25 +0800

    update dependencies

--- tests/fis-conf.js
+++ tests/fis-conf.js
@@ -3,7 +3,8 @@
 fis.match('css/*.css',{
     postprocessor : fis.plugin("autoprefixer",{
         // https://github.com/ai/browserslist#queries
-        "browsers": ['Firefox >= 20', 'Safari >= 6', 'Explorer >= 9', 'Chrome >= 12', "ChromeAndroid >= 4.0"],
+        // "browsers": ['Firefox >= 20', 'Safari >= 6', 'Explorer >= 9', 'Chrome >= 12', "ChromeAndroid >= 4.0"],
+        "browsers": ['last 2 iOS major versions'],
         "flexboxfixer": true,
         "gradientfixer": true
     })
